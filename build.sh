c2511e309a49770938bb0bfb856f96d6c8c70d1e
ederparaiso <ederpcf@gmail.com>
2019-12-02 14:28:54 -0300

    update repo

--- build.sh
+++ build.sh
@@ -2,9 +2,7 @@
 set -eo pipefail
 
 VERSION=1.0.0
-REGISTRY="zupmeqa.azurecr.io/atomicpocs"
-PROJECT="apoc"
-IMAGE="${REGISTRY}/${PROJECT}"
+IMAGE="docker.pkg.github.com/atomicpocs/apoc/apoc"
 
 docker build -t $IMAGE:latest -t $IMAGE:$VERSION .
 docker push $IMAGE:latest
